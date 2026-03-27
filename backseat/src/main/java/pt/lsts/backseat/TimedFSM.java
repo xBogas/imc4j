52439554821eb9d7c34562459d412a7ed0748c9d
xBogas <119903183+xBogas@users.noreply.github.com>
2026-03-27 12:38:57 +0000

    backseat/src/backseat/TimedFSM: Update code style.

--- backseat/src/main/java/pt/lsts/backseat/TimedFSM.java
+++ backseat/src/main/java/pt/lsts/backseat/TimedFSM.java
@@ -6,42 +6,44 @@ import pt.lsts.imc4j.msg.FollowRefState;
 
 public class TimedFSM extends FSMController {
 
-	protected Date deadline = new Date(System.currentTimeMillis() + 3600 * 1000);
-	
-	@Override
-	public void update(FollowRefState fref) {
-		if (deadline != null && deadline.getTime() < System.currentTimeMillis()) {
-			onDeadline();
-		}
-		else
-			super.update(fref);
-	}
+    protected Date deadline = new Date(System.currentTimeMillis() + 3600 * 1000);
 
-	protected void onDeadline() {
-		print("Deadline reached, terminating.");
-		sendViaIridium("ERROR: \"Deadline reached, stopped.\"", 60);
-		sendViaSms("ERROR: \"Deadline reached, stopped.\"", 60);
-		setPaused(true);
-	}
-	
-	@Override
-	protected void printFSMState() {
-    	String method = currentThread().getStackTrace()[2].getMethodName();
-		if (deadline == null) {
-			print("FSM State: " + method + " (past deadline)");
-			return;
-		}
-        long timeMillis = deadline.getTime() - System.currentTimeMillis();
-		print(String.format("FSM State: %s (%ds left, or %dmin left, or %fh left)", method, timeMillis / 1000, timeMillis / 60000, timeMillis / 3600000d));
+    @Override
+    public void update(FollowRefState fref) {
+        if (deadline != null && deadline.getTime() < System.currentTimeMillis()) {
+            onDeadline();
+        }
+        else {
+            super.update(fref);
+        }
     }
 
-	protected void printException(Exception e) {
-		printError("Exception: " + e.getMessage());
-		String trace = java.util.Arrays.toString(e.getStackTrace());
-		printError("Trace: " + trace);
-	}
+    protected void onDeadline() {
+        print("Deadline reached, terminating.");
+        sendViaIridium("ERROR: \"Deadline reached, stopped.\"", 60);
+        sendViaSms("ERROR: \"Deadline reached, stopped.\"", 60);
+        setPaused(true);
+    }
 
-	public void setDeadline(Date date) {
-		this.deadline = date;
-	}
+    @Override
+    protected void printFSMState() {
+        String method = currentThread().getStackTrace()[2].getMethodName();
+        if (deadline == null) {
+            print("FSM State: " + method + " (past deadline)");
+            return;
+        }
+        long timeMillis = deadline.getTime() - System.currentTimeMillis();
+        print(String.format("FSM State: %s (%ds left, or %dmin left, or %fh left)", method,
+                timeMillis / 1000, timeMillis / 60000, timeMillis / 3600000d));
+    }
+
+    protected void printException(Exception e) {
+        printError("Exception: " + e.getMessage() + " - " + e);
+        String trace = java.util.Arrays.toString(e.getStackTrace());
+        printError("Trace: " + trace);
+    }
+
+    public void setDeadline(Date date) {
+        this.deadline = date;
+    }
 }
